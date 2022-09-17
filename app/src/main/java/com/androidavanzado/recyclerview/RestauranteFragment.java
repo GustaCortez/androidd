b5377f396e8f6b70c4f711e9f47c0f9624f7b893
GustaCortez <melvincortey@gmail.com>
2022-09-16 21:31:58 -0600

    Recycler

---
+++ app/src/main/java/com/androidavanzado/recyclerview/RestauranteFragment.java
@@ -0,0 +1,88 @@
+package com.androidavanzado.recyclerview;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A fragment representing a list of Items.
+ */
+public class RestauranteFragment extends Fragment {
+    MyRestauranteRecyclerViewAdapter adapterRestaurantes;
+
+    List<Restaurante> restauranteList;
+
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+
+
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public RestauranteFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static RestauranteFragment newInstance(int columnCount) {
+        RestauranteFragment fragment = new RestauranteFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_item_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            //lista de elementos
+            restauranteList =new ArrayList<>();
+            restauranteList.add(new Restaurante("pizzeria carlos","",4.0f,"Madrid, España"));
+            restauranteList.add(new Restaurante("panaderida lis","",4.0f,"Madrid, España"));
+            restauranteList.add(new Restaurante("la fonda","",4.0f,"Madrid, España"));
+            restauranteList.add(new Restaurante("saborcinto caliente","",4.0f,"Madrid, España"));
+
+
+            //asociamos el adapatador al recyclerview
+            adapterRestaurantes = new MyRestauranteRecyclerViewAdapter(getActivity(),restauranteList);
+            recyclerView.setAdapter(adapterRestaurantes);
+        }
+        return view;
+
+    }
+}
